063ae99ea3ec882cb4981e927e4cadf52209fb63
Adarsh Kumar <adarsh209865443@gmail.com>
2026-01-31 17:44:52 +0530

    resolving issue on netlify

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+// import React from 'react'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import { AuthProvider } from './context/AuthContext.jsx'
+// createRoot(document.getElementById('root')).render(
+//   <AuthProvider>
+//     <App />
+//   </AuthProvider>,
+// )
+
+
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
+
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
+  <StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </StrictMode>
 )
